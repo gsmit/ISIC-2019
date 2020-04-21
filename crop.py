c7d75b88bf0648eecf7d466ead7ff230aa9b1cdf
gsmit <g.smit@student.tue.nl>
2020-04-21 17:30:47 +0200

    Update crop and resize utilities

--- crop.py
+++ crop.py
@@ -3,6 +3,7 @@ import glob
 import numpy as np
 from pathlib import Path
 
+#%%
 crop_dir = 'D://Data//ISIC-2019//cropped//'
 images = glob.glob(crop_dir + '*.jpg')
 
@@ -68,16 +69,6 @@ def crop_image(img, tol=7):
         return img
 
 
-# for idx in range(len(images)):
-#     img = cv2.imread(images[idx])
-#     img = cv2.resize(img, (600, 600))
-#     # crop, count = crop_image_from_gray(img, tolerance=10, threshold=0.9, ratio=0.9)
-#     crop = crop_image(img, tol=30)
-#     crop = cv2.resize(crop, (600, 600))
-#     plt.imshow(np.hstack((img, crop)))
-#     plt.title(f'Image {idx+1}')
-#     plt.show()
-
 # skin lesion categories
 classes = ['MEL', 'NV', 'BCC', 'AK', 'BKL', 'DF', 'VASC', 'SCC', 'UNK']
 for label in ['MEL', 'NV', 'BCC', 'BKL', 'DF', 'VASC', 'SCC', 'UNK']:
@@ -89,3 +80,90 @@ for label in ['MEL', 'NV', 'BCC', 'BKL', 'DF', 'VASC', 'SCC', 'UNK']:
         height, width, depth = img.shape
         crop = crop_image(img, tol=30)
         cv2.imwrite(image, crop)
+
+#%%
+# skin lesion categories
+data_path = Path('D:/Data/ISIC-2019/images_test_external/')
+sub_images = [str(p) for p in data_path.rglob('*.jpg')]
+for image in sub_images:
+    img = cv2.imread(image)
+    height, width, depth = img.shape
+    crop = crop_image(img, tol=30)
+    cv2.imwrite(image, crop)
+
+
+#%%
+def center_crop(path, ratio=0.8):
+    img = cv2.imread(path)
+    height, width, depth = img.shape
+
+    # center of the image
+    h = int(np.floor(height / 2))
+    w = int(np.floor(width / 2))
+
+    # shift to border
+    y = int(np.floor(h * ratio))
+    x = int(np.floor(w * ratio))
+
+    # crop the image
+    crop = img[h-y:h+y, w-x:w+x]
+
+    return crop
+
+
+# crop unknown images
+data_path = Path('D:/Data/ISIC-2019/images_train_external_SD198/UNK_selected_cropped_40')
+sub_images = [str(p) for p in data_path.rglob('*.jpg')]
+
+for image in sub_images:
+    crop = center_crop(image, ratio=0.4)
+    cv2.imwrite(image, crop)
+
+
+#%%
+def resize_with_aspect_ratio(path, min_size=450):
+    img = cv2.imread(path)
+    height, width, depth = img.shape
+
+    if height < width:
+        ratio = width / height
+        new_height = min_size
+        new_width = int(round(min_size * ratio, 0))
+    else:
+        ratio = height / width
+        new_height = int(round(min_size * ratio, 0))
+        new_width = min_size
+
+    if min(height, width) > min_size:
+        resized = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_AREA)
+    else:
+        resized = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+
+    return resized
+
+
+# crop unknown images
+data_path = Path('D:/Data/ISIC-2019/images_train_external_600')
+sub_images = sorted([str(p) for p in data_path.rglob('*.jpg')] + [str(p) for p in data_path.rglob('*.bmp')])
+
+#%%
+for e, image in enumerate(sub_images):
+    resized = resize_with_aspect_ratio(image, min_size=450)
+    cv2.imwrite(image, resized)
+
+    if ((e + 1) % 1000 == 0) or (e + 1 == len(sub_images)):
+        print(f'Resized {e + 1} of {len(sub_images)} images')
+
+
+#%%
+
+# crop unknown images
+data_path = Path('D:/Data/ISIC-2019/images_test_external_450')
+sub_images = sorted([str(p) for p in data_path.rglob('*.jpg')] + [str(p) for p in data_path.rglob('*.bmp')])
+
+for e, image in enumerate(sub_images):
+    resized = resize_with_aspect_ratio(image, min_size=450)
+    cv2.imwrite(image, resized)
+
+    if ((e + 1) % 1000 == 0) or (e + 1 == len(sub_images)):
+        print(f'Resized {e + 1} of {len(sub_images)} images')
