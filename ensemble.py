edb789f8eb493a417273d0c9699085598a3315af
gsmit <g.smit@student.tue.nl>
2020-05-01 14:10:12 +0200

    Add B4 model

--- ensemble.py
+++ ensemble.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 # load submissions
-# baseline = pd.read_csv('D:/Data/ISIC-2019/submissions/efficient-net-b1-baseline-v3.csv', index_col='image')
+baseline = pd.read_csv('D:/Data/ISIC-2019/submissions/efficient-net-b1-baseline-v3.csv', index_col='image')
 
 efn_b3 = pd.read_csv('D:/Data/ISIC-2019/submissions/efficient-net-b3-baseline-v1.csv', index_col='image')
 
-# efn_b3_v1_augment = pd.read_csv(
-#     'D:/Data/ISIC-2019/submissions/efficient-net-b3-baseline-v1-augment.csv', index_col='image')
+efn_b3_v1_augment = pd.read_csv(
+    'D:/Data/ISIC-2019/submissions/efficient-net-b3-baseline-v1-augment.csv', index_col='image')
 
 efn_b3_v2_augment = pd.read_csv(
     'D:/Data/ISIC-2019/submissions/efficient-net-b3-baseline-v2-augment.csv', index_col='image')
@@ -20,9 +20,12 @@ efn_b3_v4_subset = pd.read_csv(
 efn_b3_v5_subset = pd.read_csv(
     'D:/Data/ISIC-2019/submissions/efficient-net-b3-baseline-v5-augment-493.csv', index_col='image')
 
+efn_b4_v1_subset = pd.read_csv(
+    'D:/Data/ISIC-2019/submissions/efficient-net-b4-baseline-v1-subset-504.csv', index_col='image')
+
 # combine submissions
-submission = efn_b3 + efn_b3_v2_augment + efn_b3_v3_augment + efn_b3_v4_subset
-submission = submission / 5
+submission = efn_b3_v1_augment + efn_b3_v2_augment + efn_b3_v3_augment + efn_b3_v4_subset + efn_b4_v1_subset
+submission = submission / 3
 
 # save new submission
-submission.to_csv('D:/Data/ISIC-2019/submissions/ensemble-v5-augment.csv', index=True)
+submission.to_csv('D:/Data/ISIC-2019/submissions/ensemble-v6-augment.csv', index=True)
