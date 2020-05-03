bfd2d128d360a5627742baacd3c19dd43efec94b
gsmit <g.smit@student.tue.nl>
2020-05-03 16:59:52 +0200

    Finalize ensemble submission

--- ensemble.py
+++ ensemble.py
@@ -1,40 +1,16 @@
 import pandas as pd
 
 # load submissions
-baseline = pd.read_csv('D:/Data/ISIC-2019/submissions/efficient-net-b1-baseline-v3.csv', index_col='image')
-
-efn_b3 = pd.read_csv('D:/Data/ISIC-2019/submissions/efficient-net-b3-baseline-v1-467.csv', index_col='image')
-
-efn_b3_v1_augment = pd.read_csv(
-    'D:/Data/ISIC-2019/submissions/efficient-net-b3-baseline-v1-augment.csv', index_col='image')
-
-efn_b3_v2_augment = pd.read_csv(
-    'D:/Data/ISIC-2019/submissions/efficient-net-b3-baseline-v2-augment-502.csv', index_col='image')
-
-efn_b3_v3_augment = pd.read_csv(
-    'D:/Data/ISIC-2019/submissions/efficient-net-b3-baseline-v3-augment-508.csv', index_col='image')
-
-efn_b3_v4_subset = pd.read_csv(
-    'D:/Data/ISIC-2019/submissions/efficient-net-b3-baseline-v4-augment-521.csv', index_col='image')
-
-efn_b3_v5_subset = pd.read_csv(
-    'D:/Data/ISIC-2019/submissions/efficient-net-b3-baseline-v5-augment-493.csv', index_col='image')
-
-efn_b4_v1_subset = pd.read_csv(
-    'D:/Data/ISIC-2019/submissions/efficient-net-b4-baseline-v1-subset-504.csv', index_col='image')
-
-efn_b4_v2_subset = pd.read_csv(
-    'D:/Data/ISIC-2019/submissions/efficient-net-b4-baseline-v2-augment-502.csv', index_col='image')
-
-efn_b4_v2_pseudo_450 = pd.read_csv(
-    'D:/Data/ISIC-2019/submissions/efficient-net-b4-baseline-v3-pseudo-450-513.csv', index_col='image')
+model_1 = pd.read_csv('D:/Data/ISIC-2019/submissions/final-efficient-net-b3-baseline-v1-augment-no-augment-450-LB-540.csv', index_col='image')
+model_2 = pd.read_csv('D:/Data/ISIC-2019/submissions/final-efficient-net-b3-baseline-v3-augment-no-augment-450-LB-537.csv', index_col='image')
+model_3 = pd.read_csv('D:/Data/ISIC-2019/submissions/final-efficient-net-b3-baseline-v4-augment-no-augment-450-LB-534.csv', index_col='image')
+model_4 = pd.read_csv('D:/Data/ISIC-2019/submissions/final-efficient-net-b3-baseline-v5-augment-no-augment-450-LB-537.csv', index_col='image')
+model_5 = pd.read_csv('D:/Data/ISIC-2019/submissions/final-efficient-net-b4-baseline-v1-augment-no-augment-450-LB-544.csv', index_col='image')
+model_6 = pd.read_csv('D:/Data/ISIC-2019/submissions/final-efficient-net-b4-baseline-v10-pseudo-no-augment-450-LB-540.csv', index_col='image')
 
 # combine submissions
-# submission = efn_b3_v1_augment + efn_b3_v2_augment + efn_b3_v3_augment + efn_b3_v4_subset + efn_b4_v1_subset + efn_b4_v2_subset
-# submission = submission / 6
-
-submission = efn_b3_v3_augment + efn_b3_v4_subset + efn_b4_v2_pseudo_450
-submission = submission / 3
+submission = model_1 + model_2 + model_3 + model_4 + model_5 + model_6
+submission = submission / 2
 
 # save new submission
-submission.to_csv('D:/Data/ISIC-2019/submissions/ensemble-450-v1-augment.csv', index=True)
+submission.to_csv('D:/Data/ISIC-2019/submissions/final-ensemble-B4.csv', index=True)
